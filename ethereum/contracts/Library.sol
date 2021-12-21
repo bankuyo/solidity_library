cae50e0b0310db67a800dad657d9fd1dbcb70003
bankuyo <bankuyo@gmail.com>
2021-12-21 22:54:11 +0900

    starting creating react app

--- ethereum/contracts/Library.sol
+++ ethereum/contracts/Library.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity 0.8.10;
+pragma solidity 0.8.11;
 import './devs/BookManager.sol';
 import './devs/UserManager.sol';
 import './devs/TokenManager.sol';
---
+++ pages/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+class LibraryIndex extends React.Component{
+    render(){
+        return (
+            <div>Landing Page</div>
+        );
+    }
+}
+
+export default LibraryIndex;
